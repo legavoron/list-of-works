a5ece80bdda57422a4417f5b91684158dfa7632e
Aleh Shchurko <alehshchurko@gmail.com>
2022-04-17 14:29:58 +0300

    add hidden block

--- js/script.js
+++ js/script.js
@@ -1,1 +1,16 @@
-console.log('object');
+let btnShow = document.querySelector('#btn_show');
+let btnClose = document.querySelector('#btn_cls');
+let containerResult = document.querySelector('.wrapper__hideContainer');
+
+
+
+btnShow.addEventListener('click', showResult);
+btnClose.addEventListener('click', hideResult);
+
+function showResult() {
+    containerResult.style.display = 'block';
+}
+
+function hideResult() {
+    containerResult.style.display = 'none';
+}
